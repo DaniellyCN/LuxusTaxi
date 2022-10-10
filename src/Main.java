a5d7ffd95822541a40b3044ddc9524c095d9130e
DaniellyCN <89779359+DaniellyCN@users.noreply.github.com>
2022-10-10 11:29:42 -0300

    primeiro commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+
+public class Main{
+    public static void main(String[] args) {
+        Simulation simulation = new Simulation(new Location(10, 20),100, 100);
+        simulation.run();
+    }
+}
